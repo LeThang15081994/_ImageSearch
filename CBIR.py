e79c2d4d4c16e150ba91145913ce3be250fc3c4b
LeThang15081994 <lesythang1581994@gmail.com>
2024-09-21 16:45:24 +0700

    update file

--- CBIR.py
+++ CBIR.py
@@ -1,7 +1,49 @@
 import vectorization
-import os
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import math
+
+def vectors_data(data_path):
+    vectors = vectorization.feature_extract()
+    model = vectors.get_model_extract()
+    vectors.store_vector(model, data_path)
+
+def search_img(img_path):
+    image = vectorization.feature_extract()
+    model = image.get_model_extract()
+    img_search_vector = image.vector_normalized(model, img_path)
+
+    with open("vectors.pkl", "rb") as f:
+        vectors = pickle.load(f)
+    with open("paths.pkl", "rb") as f:
+        paths = pickle.load(f)
+
+    distance = np.linalg.norm(vectors - img_search_vector, axis=1)
+
+    ids = np.argsort(distance)[:10]
+    nearest_image = [(paths[id], distance[id]) for id in ids]
+
+    return nearest_image
 
 
-def store_vector(data_path):
+if __name__ == "__main__":
+    #vectors_data('dataset')
+    nearest_image = search_img('testimg/cheetah1.jpg')
+    axes = []
+    grid_size = int(math.sqrt(10))
+    fig = plt.figure(figsize=(10,5))
 
-    for img_path in os
+
+    for id in range(10-1):
+        draw_image = nearest_image[id]
+        axes.append(fig.add_subplot(grid_size, grid_size, id+1))
+
+        axes[-1].set_title(draw_image[1])
+        plt.imshow(Image.open(draw_image[0]))
+
+    fig.tight_layout()
+    plt.show()
+
+
--- vectorization.py
+++ vectorization.py
@@ -14,12 +14,12 @@ class feature_extract:
         pass
     
     def get_model_extract(self): # make a model to extract features use pretrain of VGG16
-        vgg16_model = VGG16(weights = 'imgaenet') 
+        vgg16_model = VGG16(weights = 'imagenet') 
         model_extract = Model(inputs = vgg16_model.input, outputs = vgg16_model.get_layer("fc1").output)
         return model_extract
     
     def img_preprocess(seft, img): # image preprocessing convert to tensor
-        img = image.resize((224,224)) 
+        img = img.resize((224,224)) 
         img = img.convert('RGB') 
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis = 0) # add batch_size axis
@@ -27,13 +27,33 @@ class feature_extract:
         return x
     
     def vector_normalized(self, model, img_path): # extract vector and normalized
-        print('processing...', img_path)
+        print('processing...............................................', img_path)
         img = Image.open(img_path)
         img_tensor = self.img_preprocess(img)
 
         vector = model.predict(img_tensor)[0] # get value from 2D to 1D
         vector = vector / np.linalg.norm(vector) # normalized
+        print('processed !!!', img_path)
         return vector
+    
+    def store_vector(self, model, data_path):  # new method to store vectors
+        vectors = []
+        paths = []
+
+        for img_path in os.listdir(data_path):
+            img_path_full = os.path.join(data_path, img_path)
+            img_vector = self.vector_normalized(model, img_path_full)
+
+            vectors.append(img_vector)
+            paths.append(img_path_full)
+
+        print("Saving............................................")
+        with open('vectors.pkl', 'wb') as f:
+            pickle.dump(vectors, f)
+        with open('paths.pkl', 'wb') as f:
+            pickle.dump(paths, f)
+
+        print("Vectors and paths saved.")
 
     def save_model(self, model, save_path):
         model.save(save_path)
