e318ffd8a87092afaaad8ba67b348de9ba332f1e
LeThang15081994 <lesythang1581994@gmail.com>
2024-09-23 11:36:43 +0700

    upgrade file

--- CBIR.py
+++ CBIR.py
@@ -1,4 +1,4 @@
-import vectorization
+from vectorization import feature_extract
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,12 +6,12 @@ from PIL import Image
 import math
 
 def vectors_data(data_path):
-    vectors = vectorization.feature_extract()
+    vectors = feature_extract()
     model = vectors.get_model_extract()
     vectors.store_vector(model, data_path)
 
 def search_img(img_path):
-    image = vectorization.feature_extract()
+    image = feature_extract()
     model = image.get_model_extract()
     img_search_vector = image.vector_normalized(model, img_path)
 
@@ -22,7 +22,7 @@ def search_img(img_path):
 
     distance = np.linalg.norm(vectors - img_search_vector, axis=1)
 
-    ids = np.argsort(distance)[:10]
+    ids = np.argsort(distance)[:20]
     nearest_image = [(paths[id], distance[id]) for id in ids]
 
     return nearest_image
@@ -30,17 +30,18 @@ def search_img(img_path):
 
 if __name__ == "__main__":
     #vectors_data('dataset')
-    nearest_image = search_img('testimg/fox3.jpg')
+    nearest_image = search_img('testimg/tiger3.jpg')
+
+    # show the same image with query image
     axes = []
-    grid_size = int(math.sqrt(10))
-    fig = plt.figure(figsize=(10,5))
+    grid_size = int(math.ceil(math.sqrt(20)))
+    fig = plt.figure(figsize=(10,10))
 
-
-    for id in range(10-1):
+    for id in range(20):
         draw_image = nearest_image[id]
         axes.append(fig.add_subplot(grid_size, grid_size, id+1))
 
-        axes[-1].set_title(draw_image[1])
+        axes[-1].set_title(draw_image[0] + '\n' + str(draw_image[1]))
         plt.imshow(Image.open(draw_image[0]))
 
     fig.tight_layout()
